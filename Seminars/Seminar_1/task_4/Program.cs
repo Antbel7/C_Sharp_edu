b21929ef961cff015c1d137436fd73473b7c562a
Antbel7 <antbel7@rambler.ru>
2023-07-12 22:54:43 +0300

    Solved task_6

--- Seminars/Seminar_1/task_4/Program.cs
+++ Seminars/Seminar_1/task_4/Program.cs
@@ -8,10 +8,12 @@ int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 int c = Convert.ToInt32(Console.ReadLine());
 
-if (a >= b && a >= c) {
-    Console.WriteLine( a + " максимальное число");
+if (a >= b && a >= c)
+{
+    Console.WriteLine(a + " максимальное число");
 }
-else if (b >= a && b >= c) {
-    Console.WriteLine( b + " максимальное число");
+else if (b >= a && b >= c)
+{
+    Console.WriteLine(b + " максимальное число");
 }
-else Console.WriteLine( c + " максимальное число");
+else Console.WriteLine(c + " максимальное число");
---
+++ Seminars/Seminar_1/task_6/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2 == 0)
+{
+    Console.WriteLine(a + " четное");
+}
+else Console.WriteLine(a + " нечетное");
